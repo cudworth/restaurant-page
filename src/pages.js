88fbc1267660983e3228d06b321e6d2a235db7a8
Austin Cudworth <austincudworth@gmail.com>
2020-05-17 21:37:34 -0700

    Add event listeners to nav bar links and add factory function for menu items

--- src/pages.js
+++ src/pages.js
@@ -17,12 +17,17 @@ const template = (function(){
 
         const home_link = _createElement(nav_bar, 'a', {class: 'navlink'});
         home_link.textContent = 'Home';
+        home_link.addEventListener('click', () => homePage.render(content_container));
+
 
         const menu_link = _createElement(nav_bar, 'a', {class: 'navlink'});
         menu_link.textContent = 'Menu';
+        menu_link.addEventListener('click', () => menuPage.render(content_container));
 
         const contact_link = _createElement(nav_bar, 'a', {class: 'navlink'});
         contact_link.textContent = 'Contact';
+        contact_link.addEventListener('click', () => contactPage.render(content_container));
+
 
 
         const content_container = _createElement(root, 'div', {id:'container'});
@@ -34,7 +39,7 @@ const template = (function(){
 
 const homePage = (function(){
     const render = function(parent){
-
+        parent.innerHTML = '';
         const txt = _createElement(parent, 'div', {class:'mytextbox', id:'homepage'});
         txt.textContent = lorem;
 
@@ -45,6 +50,7 @@ const homePage = (function(){
 
 const contactPage = (function(){
     const render = function(parent){
+        parent.innerHTML = '';
         const contact = {
             Name: 'John Doe',
             Address: '1 Main Street, Seattle, WA',
@@ -59,10 +65,12 @@ const contactPage = (function(){
 
 const menuPage = (function(){
     const render = function(parent){
-
+        const cheezewhiz = _menuItem('Cheese Wiz', 'Delectable, creamy cheese whiz', '$3.50');
+        cheezewhiz.render(parent);
     };
     return {render};
 })();
+
 
 const _createElement = function(parent, type, attrs){
     const child = document.createElement(type);
@@ -74,9 +82,22 @@ const _createElement = function(parent, type, attrs){
 }
 
 
+const _menuItem = function(name, desc, price){
+
+    const render = function(parent){
+        parent.innerHTML = '';
+        const menu_item = _createElement(parent, 'div', {class:'menuitem'});
+        _createElement(menu_item, 'h1',{}).textContent = name;
+        _createElement(menu_item, 'h2',{}).textContent = desc;
+        _createElement(menu_item, 'h3',{}).textContent = price;
+    }
+    return {render}
+}
+
+
 export {
     template,
     homePage,
     contactPage,
-    menuPage
+    menuPage,
 };
